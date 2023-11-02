b9f19945daa40e76daf46f64e99e22528167a392
rowdyslav <rowdyslav@gmail.com>
2023-11-02 19:57:50 +0300

    ssss

--- main.py
+++ main.py
@@ -20,14 +20,12 @@ from minecraft_launcher_lib.utils import get_installed_versions, get_minecraft_d
 from minecraft_launcher_lib.install import install_minecraft_version
 from minecraft_launcher_lib.command import get_minecraft_command
 
-from random_username.generate import generate_username
 from uuid import uuid1
 
 from subprocess import call
-from sys import argv, exit
 
 minecraft_directory = get_minecraft_directory().replace("minecraft", "rowdylauncher")
-print(get_installed_versions(minecraft_directory))
+
 
 class LaunchThread(QThread):
     launch_setup_signal = pyqtSignal(str, dict, str)
@@ -35,7 +33,7 @@ class LaunchThread(QThread):
     state_update_signal = pyqtSignal(bool)
 
     version_id = ""
-    username = ""
+    login = ""
 
     progress = 0
     progress_max = 0
@@ -45,10 +43,10 @@ class LaunchThread(QThread):
         super().__init__()
         self.launch_setup_signal.connect(self.launch_setup)
 
-    def launch_setup(self, version_name: str, version_dict: MinecraftVersionInfo | FabricMinecraftVersion, username: str):
+    def launch_setup(self, version_name: str, version_dict: MinecraftVersionInfo | FabricMinecraftVersion, login: str):
         self.version_name = version_name
         self.version_dict = version_dict
-        self.username = username
+        self.login = login
 
     def update_progress_label(self, value):
         self.progress_label = value
@@ -72,6 +70,11 @@ class LaunchThread(QThread):
         self.state_update_signal.emit(True)
 
         if "Fabric" in self.version_name:
+            installed_versions = get_installed_versions(minecraft_directory)
+            for x in installed_versions:
+                if x["id"].split('-')[-1] == self.version_dict["version"]:
+                    v = x["id"]
+
             fabric.install_fabric(
                 minecraft_version=self.version_dict["version"],
                 minecraft_directory=minecraft_directory,
@@ -82,6 +85,7 @@ class LaunchThread(QThread):
                 },
             )
         else:
+            v = self.version_dict["id"]
             install_minecraft_version(
                 versionid=self.version_dict["id"],
                 minecraft_directory=minecraft_directory,
@@ -92,18 +96,15 @@ class LaunchThread(QThread):
                 },
             )
 
-        if self.username == "":
-            self.username = generate_username()[0]
-
         options: MinecraftOptions = {
-            "username": self.username,
+            "login": self.login,
             "uuid": str(uuid1()),
             "token": "",
         }
 
         call(
             get_minecraft_command(
-                version=self.version_dict.get("id") or self.version_dict.get("version"),
+                version=v,
                 minecraft_directory=minecraft_directory,
                 options=options,
             )
@@ -115,20 +116,24 @@ class MainWindow(QMainWindow):
         super().__init__()
 
         self.resize(512, 512)
+
         self.centralwidget = QWidget(self)
+        self.setCentralWidget(self.centralwidget)
 
         self.logo = QLabel(self.centralwidget)
         self.logo.setMaximumSize(QSize(256, 256))
-        self.logo.setText("")
-        self.logo.setPixmap(QPixmap("assets/title.png"))
+        self.logo.setPixmap(QPixmap("assets/logo.png"))
         self.logo.setScaledContents(True)
 
-        self.titlespacer = QSpacerItem(
+        self.logospacer = QSpacerItem(
             20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding
         )
 
-        self.username = QLineEdit(self.centralwidget)
-        self.username.setPlaceholderText("Username")
+        self.login = QLineEdit(self.centralwidget)
+        self.login.setPlaceholderText("Логин")
+        self.password = QLineEdit(self.centralwidget)
+        self.password.setPlaceholderText("Пароль")
+        self.password.setEchoMode(QLineEdit.Password)
 
         self.version_select = QComboBox(self.centralwidget)
 
@@ -147,18 +152,19 @@ class MainWindow(QMainWindow):
         self.start_progress_label.setVisible(False)
 
         self.start_progress = QProgressBar(self.centralwidget)
-        self.start_progress.setProperty("value", 24)
+        self.start_progress.setProperty("value", 0)
         self.start_progress.setVisible(False)
 
         self.start_button = QPushButton(self.centralwidget)
-        self.start_button.setText("Play")
+        self.start_button.setText("Играть")
         self.start_button.clicked.connect(self.launch_game) #type: ignore
 
         self.vertical_layout = QVBoxLayout(self.centralwidget)
         self.vertical_layout.setContentsMargins(15, 15, 15, 15)
         self.vertical_layout.addWidget(self.logo, 0, Qt.AlignmentFlag.AlignHCenter)
-        self.vertical_layout.addItem(self.titlespacer)
-        self.vertical_layout.addWidget(self.username)
+        self.vertical_layout.addItem(self.logospacer)
+        self.vertical_layout.addWidget(self.login)
+        self.vertical_layout.addWidget(self.password)
         self.vertical_layout.addWidget(self.version_select)
         self.vertical_layout.addItem(self.progress_spacer)
         self.vertical_layout.addWidget(self.start_progress_label)
@@ -168,8 +174,6 @@ class MainWindow(QMainWindow):
         self.launch_thread = LaunchThread()
         self.launch_thread.state_update_signal.connect(self.state_update)
         self.launch_thread.progress_update_signal.connect(self.update_progress)
-
-        self.setCentralWidget(self.centralwidget)
 
     def state_update(self, value):
         self.start_button.setDisabled(value)
@@ -183,12 +187,14 @@ class MainWindow(QMainWindow):
 
     def launch_game(self):
         self.launch_thread.launch_setup_signal.emit(
-            self.version_select.currentText(), self.version_select.currentData(), self.username.text()
+            self.version_select.currentText(), self.version_select.currentData(), self.login.text()
         )
         self.launch_thread.start()
 
 
 if __name__ == "__main__":
+    from sys import argv, exit
+
     QApplication.setAttribute(Qt.ApplicationAttribute.AA_EnableHighDpiScaling, True)
 
     app = QApplication(argv)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+        # self.setCentralWidget(self.centralwidget)
